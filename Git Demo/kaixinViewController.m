73d82722d9fbe74e7d3bdf651e281f2c2e7a1d64
ck <myck@ckdeMacBook-Pro.local>
2018-11-14 17:16:37 +0800

    新版本3.0：本次更新新增了快乐界面

---
+++ Git Demo/kaixinViewController.m
@@ -0,0 +1,33 @@
+//
+//  kaixinViewController.m
+//  Git Demo
+//
+//  Created by ck on 2018/11/14.
+//  Copyright © 2018 ck. All rights reserved.
+//
+
+#import "kaixinViewController.h"
+
+@interface kaixinViewController ()
+
+@end
+
+@implementation kaixinViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor=[UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
